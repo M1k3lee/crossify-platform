889e84c44e26a6689e22379f97fb9d00669fee6a
M1k3lee <mikewady@gmail.com>
2025-11-07 22:07:04 +0000

    Fix CORS: Allow x-creator-address header - Added x-creator-address to allowedHeaders in CORS configuration - Fixes token creation CORS error on production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -48,7 +48,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-creator-address'],
 }));
 app.use(morgan('combined'));
 app.use(express.json());
